ecbd2eafea69122d275753752fd2d1c90242e6dc
David Wilcox <david_wilcox80@hotmail.com>
2018-07-09 08:38:56 -0500

    Add strict mode

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
+"use strict";
+
 // Enemies our player must avoid
 class Enemy {
 	constructor(x, y, speed) {
-		this.x = x;  //position on x axis
-		this.y = y;  //position on y axis
-		this.speed = speed; //for calculating how fast they crawl...eww
-	     this.sprite = 'images/enemy-bug.png';
+		this.x = x; //position on x axis
+		this.y = y; //position on y axis
+		//for calculating how fast they crawl...eww
+		this.speed = 200 + Math.floor(Math.random() * 200 + 1);
+		this.sprite = 'images/enemy-bug.png';
 	}
 	// Update the enemy's position using dt, a time delta between ticks
 	update(dt) {
-	//multiply movement by the dt parameter for smooth animation
+		//multiply movement by the dt parameter for smooth animation
 		this.x += this.speed * dt;
 		if (this.x > 505) {
 			this.x = -50;
@@ -16,7 +19,7 @@ class Enemy {
 		}
 	}
 	// Draw the enemy on the screen, required method for game
-	render () {
+	render() {
 		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 	}
 }
@@ -25,17 +28,17 @@ class Enemy {
 // a handleInput() method.
 class Player extends Enemy {
 	//set Player class propterties
-	constructor (x, y, speed) {
-		super (x, y, speed);
+	constructor(x, y, speed) {
+		super(x, y, speed);
 		this.sprite = 'images/char-boy.png';
 		this.score = 0; // for tracking the score
 		this.lives = 3; // for tracking lives remaining
 		this.liveSprite = 'images/Heart.png';
 	}
 	// draw character, score, and number of lives on screen
-	render () {
+	render() {
 		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-		ctx.font  = '20pt sans-serif';
+		ctx.font = '20pt sans-serif';
 		ctx.fillText(`Score:   ${this.score}`, 10, 40);
 		for (let i = 0; i < this.lives; i++) {
 			ctx.drawImage(Resources.get(this.liveSprite), i * 40 + 385, 5, 30, 45);
@@ -44,19 +47,23 @@ class Player extends Enemy {
 	// handle keyboard inputs to move character
 	handleInput(keypress) {
 		switch (keypress) {
-		 	case 'left':
+			case 'left':
 				this.x -= 100;
-		          break
+				break;
 			case 'right':
 				this.x += 100;
 				break;
-	          case 'down':
+			case 'down':
 				this.y += 90;
 				break;
 			case 'up':
 				this.y -= 90;
 				if (this.y === -45) {
 					this.score += 100;
+					setTimeout(() => {
+						this.x = 202;
+						this.y = 405;
+					}, 500);
 				}
 				break;
 		}
@@ -64,12 +71,6 @@ class Player extends Enemy {
 	// prevent player from moving offscreen and
 	// move player back to start if they reach the end
 	update() {
-		if (this.y === -45) {
-		     setTimeout(function() {
-			    player.x = 202;
-			    player.y = 405;
-		    }, 500);
-	    }
 		if (this.x > 402) {
 			this.x = 402;
 		}
@@ -106,13 +107,13 @@ enemyStart.forEach(function(y) {
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-    player.handleInput(allowedKeys[e.keyCode]);
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
+	player.handleInput(allowedKeys[e.keyCode]);
 });
 
 // create eventListener to handle resetting game after Game Over
